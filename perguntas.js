3f71c0e877aebe4b1d5628d3dd73be88baec9d18
viniciusr90 <143603856+viniciusr90@users.noreply.github.com>
2024-11-18 07:17:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Artilharia',
+    'O quem é o maior artilheiro do brasileirão?',
+    'Qual é o maior assistente?'
+)
+
+criaCartao(
+    'Torcida',
+    'Qual é a maior torcida?',
+    'Quantos torcedores?'
+)
+
+criaCartao(
+    'Maior ganhador de premios?',
+    'Maior revelador de promeças?',
+    'Maior promeça do Brasileirão'
+)
+
+criaCartao(
+    'Melhor estrangeiro?',
+    'Qual clube atuou?',
+    'Quantos gols?'
+)
